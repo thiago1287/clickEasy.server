db2f62741b821e8e6fc6a121bdf26b33f1c4dd59
Thiago <thiagosilva1@unipam.edu.br>
2023-10-11 16:30:08 -0300

    apiAgendamento

--- src/Controllers/ScheduleController.js
+++ src/Controllers/ScheduleController.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const { prisma } = require("../services/prisma");
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    req.body
-    res.send("Teste schedule!")
-})
+
+router.post("/agendamento", async (req, res) => {
+    try {
+        const { userId, dia, horaInicio, horaFim } = req.body
+        //recuperar o userId
+        const user = await Name.findById(UserId).select('name');
+
+        //criar horario
+        const schedule = await prisma.schedule.create({
+            data: {
+                userId
+                dia,
+                horaInicio,
+                horaFim
+            },
+        });
+
+        try {
+            res.status(201).json({ msg: 'Horario Registrado' })
+            return schedule;
+        } catch (error) {
+            res.status(500).json({ msg: error })
+
+        }
+
+    });
+    } catch (err) {
+    res.json({ error: true.message: err.message })
+}
+
 module.exports = router;
