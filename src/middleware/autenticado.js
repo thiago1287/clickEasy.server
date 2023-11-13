ebb6de14bcb9680b9e4a9b2519850230241e6ae6
JoaoPedroSMB <129996011+JoaoPedroSMB@users.noreply.github.com>
2023-11-13 17:33:49 -0300

    nao terminado

--- src/middleware/autenticado.js
+++ src/middleware/autenticado.js
@@ -2,6 +2,9 @@ module.exports = async(req,res,next)=>{
     const token = req.headers.authorization
 
     if (token){
-        
+        res.status(401).send('Access token nao informado')
     }
+
+    const[,acessToken] = token.split(" ")
+
 }
