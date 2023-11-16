2393430c36a586c7e999de78001728562a5020b7
JoaoPedroSMB <129996011+JoaoPedroSMB@users.noreply.github.com>
2023-11-16 15:35:13 -0300

    2 parte do jwt

--- src/middleware/autenticado.js
+++ src/middleware/autenticado.js
@@ -1,10 +1,33 @@
-module.exports = async(req,res,next)=>{
+
+import pkg from 'jsonwebtoken';
+const { verify, decode } = pkg;
+
+
+import { secret } from '../config/jsonSecret.js';
+
+
+export default async(req,res,next)=>{
     const token = req.headers.authorization
 
-    if (token){
-        res.status(401).send('Access token nao informado')
+    if (!token){
+        return res.status(401).send('Access token nao informado')
     }
 
     const[,acessToken] = token.split(" ")
 
-}
+    try{
+        verify(acessToken,secret.secret)
+
+        const {id,email} = await decode(acessToken)
+
+        req.usuarioId = id
+        req.usuarioEmail = email
+
+        return next()
+    }  
+    catch(error){
+        res.status(401).send('Usario não autorizado')
+    }
+    }
+
+    
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import agendamentos from "./agendamentoRoutes.js"
 import register from "./registerRoutes.js";
+import auth from "./authRoutes.js"
 import cors from "cors";
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("API em execução"));
 
-    app.use(express.json(), register, agendamentos);
+    app.use(express.json(), auth, register, agendamentos);
 
     app.use(cors);
 };
