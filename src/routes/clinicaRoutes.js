346f60fe97e11cb7d23ac4c4599f9c7fc59f6b3b
Thiago <thiagosilva1@unipam.edu.br>
2023-11-17 14:05:41 -0300

    ..

--- src/routes/clinicaRoutes.js
+++ src/routes/clinicaRoutes.js
@@ -5,7 +5,7 @@ const routes = express.Router()
 
 routes.get("/clinica", ClinicaController.listarClinicas);
 routes.get("/clinica/:id", ClinicaController.listarClinicaPorId);
-routes.post("/clinica/pscio", ClinicaController.cadastrarClinicaPsico);
+routes.post("/clinica/psico", ClinicaController.cadastrarClinicaPsico);
 routes.post("/clinica/odonto", ClinicaController.cadastrarClinicaOdonto);
 routes.post("/clinica/fisio", ClinicaController.cadastrarClinicaFisio);
 routes.put("/clinica/:id", ClinicaController.atualizarClinica);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import agendamentos from "./agendamentoRoutes.js"
 import register from "./registerRoutes.js";
+import clinica from "./clinicaRoutes.js"
 import auth from "./authRoutes.js"
 import cors from "cors";
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("API em execução"));
 
-    app.use(express.json(), auth, register, agendamentos);
+    app.use(express.json(), auth, register, agendamentos, clinica);
 
     app.use(cors);
 };
